a37fd65dc4746143963f7ffcadafbd270706f693
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-11-24 19:56:34 +0100

    Initial scripts.js code

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+const canvasElement = document.querySelector('canvas');
+
+const game = new Game(canvasElement);
+
+const triggerPlayElement = document.getElementById('trigger-play');
+const triggerPlayAgainElement = document.getElementById('trigger-play-again');
+
+const screenStartElement = document.getElementById('screen-start');
+const screenSuccessEndElement = document.getElementById ('screen-passed-test');
+const screenGameOverElement = document.getElementById('screen-failed-test');
+const screenPlayElement = document.getElementById('screen-play');
+
+triggerPlayElement.addEventListener('click', ()=>{
+    screenStartElement.style.display = "none";
+    screenPlayElement.style.display = "initial";
+
+    game.loop();
+});
+
+triggerPlayAgainElement.addEventListener('click', () =>{
+    screenGameOverElement.style.display = "none";
+    screenPlayElement.style.display = "initial";
+
+    game.reset();
+    game.loop();
+}
