50ade2311346f364e6d01ce43e2e7122c42120fb
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-11-25 16:25:27 +0100

    Testing if game console.log works

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,49 @@
-const canvasElement = document.querySelector('canvas');
+const canvas = document.getElementById('play-canvas');
+const context = canvas.getContext('2d');
 
-const game = new Game(canvasElement);
 
-const triggerPlayElement = document.getElementById('trigger-play');
-const triggerPlayAgainElement = document.getElementById('trigger-play-again');
 
-const screenStartElement = document.getElementById('screen-start');
-const screenSuccessEndElement = document.getElementById ('screen-passed-test');
-const screenGameOverElement = document.getElementById('screen-failed-test');
-const screenPlayElement = document.getElementById('screen-play');
+class Game {
+    constructor(game, x, y){
+        game = this;
+        this.x = x;
+        this.y = y; 
+    }
+    runLogic(){
+     console.log("test");
+    };
+    drawImage(){
 
-triggerPlayElement.addEventListener('click', ()=>{
-    screenStartElement.style.display = "none";
-    screenPlayElement.style.display = "initial";
+    };
+}
 
-    game.loop();
-});
+const game = new Game(canvas);
+game.runLogic();
 
-triggerPlayAgainElement.addEventListener('click', () => {
-    screenGameOverElement.style.display = "none";
-    screenPlayElement.style.display = "initial";
 
-    game.reset();
-    game.loop();
-});
+//const canvasElement = document.querySelector('canvas');
+
+//const game = new Game(canvasElement);
+
+//const triggerPlayElement = document.getElementById('trigger-play');
+//const triggerPlayAgainElement = document.getElementById('trigger-play-again');
+
+//const screenStartElement = document.getElementById('screen-start');
+//const screenSuccessEndElement = document.getElementById ('screen-passed-test');
+//const screenGameOverElement = document.getElementById('screen-failed-test');
+//const screenPlayElement = document.getElementById('screen-play');
+
+//triggerPlayElement.addEventListener('click', ()=>{
+    //screenStartElement.style.display = "none";
+    //screenPlayElement.style.display = "initial";
+
+    //game.loop();
+//});
+
+//triggerPlayAgainElement.addEventListener('click', () => {
+   // screenGameOverElement.style.display = "none";
+    //screenPlayElement.style.display = "initial";
+
+    //game.reset();
+    //game.loop();
+//});
