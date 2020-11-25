a55538141c9593cfd1e3177237072066470e0226
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-11-25 23:09:37 +0100

    Words runLogic and drawWords methods

--- js/words.js
+++ js/words.js
@@ -1,29 +1,40 @@
 class Words {
-    constructor(game, x, y, xSpeed, word, points){
+    constructor(game, x, y, xSpeed){
      this.game = game;
      this.x = x;
      this.y = y;
      this.xSpeed = xSpeed;
      this.word = word;
-     this.points = points;
+     this.points = 0;
 
      this.position= 0;
     }
 
     }
-    runLogic(){
-    this.x -= this.xSpeed;
-    if(this.x < = 0){this.x += this.xSpeed};
-    if(this.x > this.canvas.width){this.x -= this.xSpeed};
+    //runLogic(){
+    //this.x -= this.xSpeed;
+    //if(this.x < = 0){this.x += this.xSpeed};
+    //if(this.x > this.canvas.width){this.x -= this.xSpeed};
     // need to deduct length of word
-}
-    holidayWords(){
-        context.fillStyle = "green";
-        context.strokeText = (Urlaub, 100, 50);
+    }
 
-    studyWords(){
+    runLogicHolidayWords(){
+        this.x -= this.xSpeed;
+        if(this.x <=0){this.x += this.xSpeed};
+    };
+
+    runLogicStudyWords(){
+        this.x += this.xSpeed;
+        if(this.x +100 > canvas.width){this.x -=this.xSpeed};
+    };
+
+    drawHolidayWords(){
+        context.fillStyle = "green";
+        context.fillText = (Urlaub, this.x, this.y, 100);
+
+    drawStudyWords(){
         context.fillStyle = "red";
-        context.strokeText = (Lernen, 100, 50);
+        context.strokeText = (Lernen, this.x, this.y, 100);
     }
     
 }
