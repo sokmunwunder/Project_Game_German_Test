974530f9a12b6eba12587533391f3d1bfeea8459
Sok Mun Wunder <sokmunwunder@gmail.com>
2020-11-28 11:18:28 +0100

    draw method in game js

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ class Game {
     }
 
     reset(){
-        this.Player = new Player(this, this.canvas.width/2, this.canvas.height/2, 50,50);
+        this.Player = new Player(this, this.canvas.width/2+50/2, this.canvas.height/2+50/2, 50,50);
         this.score = 0;
         this.words = [];
         this.wordStartingSpeed = 1;
@@ -47,8 +47,9 @@ class Game {
             this.canvas.width,
             this.y,
             this.wordStartingSpeed,
-        )
-
+        );
+        this.words.push(word);
+    }
      //const currentTimeStamp = Date.now();
      //if(
       //currentTimeStamp >
@@ -61,7 +62,7 @@ class Game {
        // if (this.x = 0){
        // this.x += this.xSpeed * (this.wordMultiplyingSpeed *=1.01);
        // }
-     {
+     //{
         // const word = new Words(
         // this,
         // this.canvas.width,
@@ -71,8 +72,8 @@ class Game {
         // );
         // this.words.push(word);
         // this.lastWordTimeStamp = currentTimeStamp;
-     }
-    }
+     //}
+   
 
    loop(){
        this.runLogic();
@@ -80,11 +81,24 @@ class Game {
    }
     runLogic(){
     console.log("test");
+    this.addWords();
+    for (let word of this.words){
+        word.runLogic();
     }
 
     draw(){
-    console.log('draw');
+    this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
     }
+    for (let word of this.words){
+        word.drawWordsLogic();
+    }
+    this.player.draw();
+
+    }
+
+    //draw(){
+    //console.log('draw');
+    //}
 
     }
     
